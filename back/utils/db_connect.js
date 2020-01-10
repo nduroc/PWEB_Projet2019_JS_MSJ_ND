fad7b1b735c7263cea69d36b454394db8f38d312
nduroc <nicolas.duroc@etu.u-bordeaux.fr>
2020-01-10 18:08:35 +0100

    Commenter db.connect

--- back/utils/db_connect.js
+++ back/utils/db_connect.js
@@ -13,15 +13,15 @@ const db = mysql.createConnection({
  */
 function querySqlSelect(sqlQuery) {
   return new Promise(function(resolve, reject) {
-    db.connect(function(err) {
-      if (err) throw err;
+    //db.connect(function(err) {
+      //if (err) throw err;
       db.query(sqlQuery, function(err, result) {
         if (err) throw err;
         console.log(result)
         resolve(result);
       });
     });
-  });
+  //});
 }
 
 /**
@@ -30,28 +30,28 @@ function querySqlSelect(sqlQuery) {
  */
 function querySqlInsert(sqlQuery) {
   return new Promise(function(resolve, reject) {
-    db.connect(function(err) {
-      if (err) throw err;
+    //db.connect(function(err) {
+      //if (err) throw err;
       db.query(sqlQuery, function(err, result) {
         if (err) throw err;
         console.log(result)
         resolve(result.insertId);
       });
     });
-  });
+  //});
 }
 
 function querySqlUpdate(sqlQuery) {
   return new Promise(function(resolve, reject) {
-    db.connect(function(err) {
-      if (err) throw err;
+    //db.connect(function(err) {
+      //if (err) throw err;
       db.query(sqlQuery, function(err, result) {
         if (err) throw err;
         console.log(result)
         resolve(result.affectedRows);
       });
     });
-  });
+  //});
 }
 
 
