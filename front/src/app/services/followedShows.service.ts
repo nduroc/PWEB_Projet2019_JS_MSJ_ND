8140d2ba775d7778121fb569cb24f42df9f2f554
Mathieu San Juan <msjuan@emi.u-bordeaux1.fr>
2020-01-17 11:47:42 +0100

    test unmark

--- front/src/app/services/followedShows.service.ts
+++ front/src/app/services/followedShows.service.ts
@@ -57,7 +57,7 @@ export class FollowedShowsService {
   unMarkEpisode(episodeId: number, userId: number,showId:number) {
     let promise = new Promise((resolve, reject) => {
 
-      this.httpClient.post(environment.apiPath + 'episode/unmark?userId=' + userId,{})
+      this.httpClient.post(environment.apiPath + 'episode/unmark?userId=' + 1 + '&episodeId=' + 1 +'&serieId=' + 1 ,{})
         .toPromise()
         .then(
           (result) => {
